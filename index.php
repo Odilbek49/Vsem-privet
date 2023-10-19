db3aade15faf861b4c9ae136a1465d605a1987c5
Odilbek <lezginodilbek@gmail.com>
2023-10-19 15:57:52 +0500

    r4fewewfew

--- index.php
+++ index.php
@@ -1,59 +1,19 @@
 <?php
-echo "hello world!\n";
-
-$name = 'Odilbek';
-
-echo "hello, $name";
-
-const NAME = 'asd';
-// global scope
-$x = 5;
-function myTest()
-{
-    global $x;
-
-    // using x inside this function will generate an error
-    echo "<p>Variable x inside function is: $x</p>";
-}
-myTest();
-
-echo "<p>Variable x outside function is: $x</p>";
-
-
-function a()
-{
-    static $a = 0;
-    echo $a;
-    $a++;
-}
-
-a();
-a();
-a();
-
-$count = 0;
-
-
-function increment(&$c) {
-    $c++;
-}
-
-increment($count);
-increment($count);
-print("\n". $count);
-
-print("\nasd");
-
-print(34);
-echo "\n";
-print(34.43 . "\n");
-
-print(34.43);
-var_dump(34);
-var_dump(["sdf", 34.4]);
-var_dump(array(434,34,34,));
 class Car {
-
+  public $color;
+  public $model;
+  public function __construct($color, $model) {
+    $this->color = $color;
+    $this->model = $model;
+  }
+  public function message() {
+    return "My car is a " . $this->color . " " . $this->model . "!";
+  }
 }
-var_dump(new Car);
-var_dump(null);
+
+$myCar = new Car("black", "Volvo");
+echo $myCar -> message();
+echo "<br>";
+$myCar = new Car("red", "Toyota");
+echo $myCar -> message();
+?>
