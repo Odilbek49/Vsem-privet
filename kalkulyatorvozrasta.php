3a310d49a64872142792250c9989a07cf6aa1afb
Odilbek <lezginodilbek@gmail.com>
2023-10-21 15:33:34 +0500

    yrf

--- kalkulyatorvozrasta.php
+++ kalkulyatorvozrasta.php
@@ -3,3 +3,6 @@ $a = readline( 'asd; ');
 
 echo (int) 2023-$a;
 ?>
+
+<!-- resulte asd; 14                                                                                                                  
+            2009 -->
