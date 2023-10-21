a2844a1e8e58b49512318c3d160d0b305b48dcef
Sasha Raimov <crazyysasha@gmail.com>
2023-10-21 16:06:46 +0500

    fix

---
+++ math.php
@@ -0,0 +1,25 @@
+<?php
+
+echo pi();
+echo "\n";
+echo (min(12, 456, 234, 56645, 0, -23));
+echo "\n";
+echo (max(12, 456, 234, 56645, 0, -23));
+echo "\n";
+echo abs(-234);
+echo "\n";
+echo abs(234);
+echo "\n";
+echo sqrt(64);
+echo "\n";
+echo round(34.43);
+echo "\n";
+echo rand(1, 2);
+
+echo "\n";
+define('MY_CONST', 'asdd');
+// define('GREAT', 'hello', true);
+
+echo MY_CONST;
+
+// echo great;
