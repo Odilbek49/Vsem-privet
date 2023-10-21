54edaf6954850f776e669794a6f1a59e44c9c4a3
Odilbek <lezginodilbek@gmail.com>
2023-10-21 15:36:50 +0500

    6yy5

--- kalkulyatorvozrasta.php
+++ kalkulyatorvozrasta.php
@@ -4,5 +4,3 @@ $a = readline( 'asd; ');
 echo (int) 2023-$a;
 ?>
 
-<!-- resulte asd; 14                                                                                                                  
-            2009 -->
